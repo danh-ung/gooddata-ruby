ca5596d5bc47445b71d901bdb23185bffad924f1
danh.ung <danh.ung@gooddata.com>
2022-01-05 19:18:04 +0700

    TRIVIAL: import directly to gooddata_brick_base

--- lib/gooddata_brick_base.rb
+++ lib/gooddata_brick_base.rb
@@ -3,6 +3,15 @@
 # Copyright (c) 2010-2017 GoodData Corporation. All rights reserved.
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
+#
+
+require 'pmap'
+$pmap_default_thread_count = 20 # rubocop:disable GlobalVars
+
+# GoodData Module
+module GoodData
+end
+
 require_relative 'gooddata/bricks/utils'
 require_relative 'gooddata/bricks/brick'
 require_relative 'gooddata/bricks/pipeline_base'
