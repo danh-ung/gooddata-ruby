54d511b71b73de5d72bd042957df9d460b668b0d
danh.ung <danh.ung@gooddata.com>
2022-01-05 18:02:46 +0700

    TRIVIAL: import directly to gooddata_brick_base

--- lib/gooddata_brick_base.rb
+++ lib/gooddata_brick_base.rb
@@ -4,5 +4,6 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 require_relative 'gooddata/bricks/utils'
+require_relative 'gooddata/bricks/brick'
 require_relative 'gooddata/bricks/middleware/base_middleware'
 require_relative 'gooddata/bricks/middleware/bench_middleware'
